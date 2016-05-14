ce4bba3efcf1bf59d0feb49006b7b7234980e265
amit-sgwn <y13uc022@lnmiit.ac.in>
2016-05-14 12:46:03 +0530

    Added a add task activity and a task class to define the complete task

--- app/src/main/java/com/taskmanager/sangwan/taskmanager/MainActivity.java
+++ app/src/main/java/com/taskmanager/sangwan/taskmanager/MainActivity.java
@@ -1,13 +1,29 @@
 package com.taskmanager.sangwan.taskmanager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button add,view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        add = (Button)findViewById(R.id.button);
+        view =(Button)findViewById(R.id.button2);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/taskmanager/sangwan/taskmanager/Task.java
@@ -0,0 +1,7 @@
+package com.taskmanager.sangwan.taskmanager;
+
+/**
+ * Created by User on 14-05-2016.
+ */
+public class Task {
+}
